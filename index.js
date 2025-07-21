88575dd1155fa995910cde98990286607d76a448
yamano <yamano@oec.co.jp>
2025-07-22 01:08:20 +0900

    feature branch create

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 console.log("Hello, World!");
-<<<<<<< HEAD
 function greet() {
   console.log("Hello from the main branch!");
 }
-=======
-console.log("疑似コンフリクト");
->>>>>>> 713602b (コンフリクト)
+
+greet();
